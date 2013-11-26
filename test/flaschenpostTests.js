44402153212e18ff27b0ea63e6ca9226acde508a
Golo Roden <webmaster@goloroden.de>
2013-11-26 16:21:58 +0100

    Removed dependency to Winston and introduced a stream-based logger.

--- test/flaschenpostTests.js
+++ test/flaschenpostTests.js
@@ -3,8 +3,7 @@
 var assert = require('node-assertthat'),
     cases = require('cases');
 
-var flaschenpost = require('../lib/flaschenpost'),
-    TestTransport = require('./TestTransport');
+var flaschenpost = require('../lib/flaschenpost');
 
 suite('flaschenpost', function () {
   suite('getLogger', function () {
@@ -25,13 +24,39 @@ suite('flaschenpost', function () {
     });
   });
 
+  test('returns an object with logging functions.', function () {
+    var logger = flaschenpost.getLogger({
+      module: 'flaschenpost',
+      version: '0.0.1'
+    });
+
+    assert.that(logger, is.ofType('object'));
+    assert.that(logger.fatal, is.ofType('function'));
+    assert.that(logger.error, is.ofType('function'));
+    assert.that(logger.warn, is.ofType('function'));
+    assert.that(logger.info, is.ofType('function'));
+    assert.that(logger.debug, is.ofType('function'));
+  });
+
+  test('pipes to the registered writable streams.', function (done) {
+    flaschenpost.once('readable', function () {
+      flaschenpost.read();
+      done();
+    });
+
+    var logger = flaschenpost.getLogger({
+      module: 'flaschenpost',
+      version: '0.0.1'
+    });
+    logger.info('1fd68e8d-10d0-4f56-b30d-6b88c02d1012', 'foo');
+  });
+
   suite('when not running on a node', function () {
     test('does not put the node object into a log message.', function (done) {
-      flaschenpost.add(TestTransport, function (level, message) {
-        assert.that(level, is.equalTo('info'));
-        var messageAsJson = JSON.parse(message);
-        assert.that(messageAsJson.node, is.undefined());
-        flaschenpost.remove(TestTransport);
+      flaschenpost.once('readable', function () {
+        var message = flaschenpost.read();
+        assert.that(message.level, is.equalTo('info'));
+        assert.that(message.node, is.undefined());
         done();
       });
 
@@ -39,6 +64,7 @@ suite('flaschenpost', function () {
         module: 'flaschenpost',
         version: '0.0.1'
       });
+
       logger.info('1fd68e8d-10d0-4f56-b30d-6b88c02d1012', 'foo', { bar: 'baz' });
     });
   });
@@ -52,47 +78,19 @@ suite('flaschenpost', function () {
       });
     });
 
-    test('create returns an object with log functions.', function () {
-      var logger = flaschenpost.getLogger({
-        module: 'flaschenpost',
-        version: '0.0.1'
-      });
-
-      assert.that(logger, is.ofType('object'));
-      assert.that(logger.fatal, is.ofType('function'));
-      assert.that(logger.error, is.ofType('function'));
-      assert.that(logger.warn, is.ofType('function'));
-      assert.that(logger.info, is.ofType('function'));
-      assert.that(logger.debug, is.ofType('function'));
-    });
-
-    test('calls all registered transports on log.', function (done) {
-      flaschenpost.add(TestTransport, function (/*level, message, metadata*/) {
-        flaschenpost.remove(TestTransport);
-        done();
-      });
-
-      var logger = flaschenpost.getLogger({
-        module: 'flaschenpost',
-        version: '0.0.1'
-      });
-      logger.info('1fd68e8d-10d0-4f56-b30d-6b88c02d1012', 'foo');
-    });
-
     test('logs all required information.', function (done) {
-      flaschenpost.add(TestTransport, function (level, message) {
-        assert.that(level, is.equalTo('info'));
-        var messageAsJson = JSON.parse(message);
-        assert.that(messageAsJson.id, is.ofType('string'));
-        assert.that(messageAsJson.id.length, is.equalTo(40));
-        assert.that(messageAsJson.timestamp, is.ofType('string'));
-        assert.that(messageAsJson.module, is.equalTo('flaschenpost@0.0.1'));
-        assert.that(messageAsJson.node, is.equalTo({ host: 'localhost', port: 3000, id: '12a30e3632a51fdab4fedd07bcc219b433e17343' }));
-        assert.that(messageAsJson.uuid, is.equalTo('1fd68e8d-10d0-4f56-b30d-6b88c02d1012'));
-        assert.that(messageAsJson.level, is.equalTo('info'));
-        assert.that(messageAsJson.message, is.equalTo('foo'));
-        assert.that(messageAsJson.metadata, is.equalTo({ bar: 'baz' }));
-        flaschenpost.remove(TestTransport);
+      flaschenpost.once('readable', function () {
+        var message = flaschenpost.read();
+        assert.that(message.level, is.equalTo('info'));
+        assert.that(message.id, is.ofType('string'));
+        assert.that(message.id.length, is.equalTo(40));
+        assert.that(message.timestamp, is.ofType('number'));
+        assert.that(message.module, is.equalTo('flaschenpost@0.0.1'));
+        assert.that(message.node, is.equalTo({ host: 'localhost', port: 3000, id: '12a30e3632a51fdab4fedd07bcc219b433e17343' }));
+        assert.that(message.uuid, is.equalTo('1fd68e8d-10d0-4f56-b30d-6b88c02d1012'));
+        assert.that(message.level, is.equalTo('info'));
+        assert.that(message.message, is.equalTo('foo'));
+        assert.that(message.metadata, is.equalTo({ bar: 'baz' }));
         done();
       });
 
@@ -104,19 +102,19 @@ suite('flaschenpost', function () {
     });
 
     test('omits metadata when no metadata are given.', function (done) {
-      flaschenpost.add(TestTransport, function (level, message) {
-        assert.that(level, is.equalTo('info'));
-        var messageAsJson = JSON.parse(message);
-        assert.that(messageAsJson.id, is.ofType('string'));
-        assert.that(messageAsJson.id.length, is.equalTo(40));
-        assert.that(messageAsJson.timestamp, is.ofType('string'));
-        assert.that(messageAsJson.module, is.equalTo('flaschenpost@0.0.1'));
-        assert.that(messageAsJson.node, is.equalTo({ host: 'localhost', port: 3000, id: '12a30e3632a51fdab4fedd07bcc219b433e17343' }));
-        assert.that(messageAsJson.uuid, is.equalTo('1fd68e8d-10d0-4f56-b30d-6b88c02d1012'));
-        assert.that(messageAsJson.level, is.equalTo('info'));
-        assert.that(messageAsJson.message, is.equalTo('foo'));
-        assert.that(messageAsJson.metadata, is.undefined());
-        flaschenpost.remove(TestTransport);
+      flaschenpost.once('readable', function () {
+        var message = flaschenpost.read();
+
+        assert.that(message.level, is.equalTo('info'));
+        assert.that(message.id, is.ofType('string'));
+        assert.that(message.id.length, is.equalTo(40));
+        assert.that(message.timestamp, is.ofType('number'));
+        assert.that(message.module, is.equalTo('flaschenpost@0.0.1'));
+        assert.that(message.node, is.equalTo({ host: 'localhost', port: 3000, id: '12a30e3632a51fdab4fedd07bcc219b433e17343' }));
+        assert.that(message.uuid, is.equalTo('1fd68e8d-10d0-4f56-b30d-6b88c02d1012'));
+        assert.that(message.level, is.equalTo('info'));
+        assert.that(message.message, is.equalTo('foo'));
+        assert.that(message.metadata, is.undefined());
         done();
       });
 
@@ -128,11 +126,10 @@ suite('flaschenpost', function () {
     });
 
     test('logs errors correctly.', function (done) {
-      flaschenpost.add(TestTransport, function (level, message) {
-        var messageAsJson = JSON.parse(message);
-        assert.that(messageAsJson.metadata.name, is.equalTo('Error'));
-        assert.that(messageAsJson.metadata.message, is.equalTo('foo'));
-        flaschenpost.remove(TestTransport);
+      flaschenpost.once('readable', function () {
+        var message = flaschenpost.read();
+        assert.that(message.metadata.name, is.equalTo('Error'));
+        assert.that(message.metadata.message, is.equalTo('foo'));
         done();
       });
 
@@ -144,13 +141,12 @@ suite('flaschenpost', function () {
     });
 
     test('logs errors correctly when they appear recursively.', function (done) {
-      flaschenpost.add(TestTransport, function (level, message) {
-        var messageAsJson = JSON.parse(message);
-        assert.that(messageAsJson.metadata, is.ofType('object'));
-        assert.that(messageAsJson.metadata.error, is.ofType('object'));
-        assert.that(messageAsJson.metadata.error.name, is.equalTo('Error'));
-        assert.that(messageAsJson.metadata.error.message, is.equalTo('foo'));
-        flaschenpost.remove(TestTransport);
+      flaschenpost.once('readable', function () {
+        var message = flaschenpost.read();
+        assert.that(message.metadata, is.ofType('object'));
+        assert.that(message.metadata.error, is.ofType('object'));
+        assert.that(message.metadata.error.name, is.equalTo('Error'));
+        assert.that(message.metadata.error.message, is.equalTo('foo'));
         done();
       });
 
@@ -168,9 +164,9 @@ suite('flaschenpost', function () {
       [ 'info' ],
       [ 'debug' ]
     ], function (level, done) {
-      flaschenpost.add(TestTransport, function (receivedLevel) {
-        assert.that(receivedLevel, is.equalTo(level));
-        flaschenpost.remove(TestTransport);
+      flaschenpost.once('readable', function () {
+        var message = flaschenpost.read();
+        assert.that(message.level, is.equalTo(level));
         done();
       });
 
@@ -188,7 +184,7 @@ suite('flaschenpost', function () {
       });
       assert.that(function () {
         logger.info('foo', { bar: 'baz' });
-      }, is.throwing());
+      }, is.throwing('UUID is missing.'));
     });
 
     test('throws an error if no uuid is given and no metadata are given.', function () {
@@ -198,7 +194,7 @@ suite('flaschenpost', function () {
       });
       assert.that(function () {
         logger.info('foo');
-      }, is.throwing());
+      }, is.throwing('UUID is missing.'));
     });
   });
 
