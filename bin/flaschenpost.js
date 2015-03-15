53869752a5b580a43932bee5eb5b7f7cf45d18a0
Golo Roden <webmaster@goloroden.de>
2015-03-15 12:22:38 +0100

    Re-enabled update-notifier in flaschenpost CLI.

--- bin/flaschenpost.js
+++ bin/flaschenpost.js
@@ -2,6 +2,14 @@
 
 'use strict';
 
-var flaschenpost = require('../lib/flaschenpost');
+var updateNotifier = require('update-notifier');   +var flaschenpost = require('../lib/flaschenpost');
+
+var flaschenpost = require('../lib/flaschenpost'),
+    packageJson = require('../package.json');
+
+updateNotifier({
+  packageName: packageJson.name,
+  packageVersion: packageJson.version
+}).notify();
 
 flaschenpost.uncork(process.stdin, process.stdout);
