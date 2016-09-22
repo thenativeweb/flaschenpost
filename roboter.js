4609a2748249afcbeaad65017f74d2e56418a581
Golo Roden <webmaster@goloroden.de>
2016-09-22 13:06:01 +0200

    Fix paths.

--- roboter.js
+++ roboter.js
@@ -8,14 +8,5 @@ roboter.
     task('universal/analyze', {
       src: [ '**/*.js', '!node_modules/**/*.js', '!coverage/**/*.js' ]
     });
-
-    task('universal/test-units', {
-      src: 'test/**/*Tests.js'
-    });
-
-    task('universal/coverage', {
-      src: 'lib/**/*.js',
-      test: 'test/**/*Tests.js'
-    });
   }).
   start();
