a8808cc9dfe4f377a72f51d01cdcf85fce785318
Golo Roden <golo.roden@thenativeweb.io>
2017-03-02 13:51:43 +0100

    Make formatters work for uncork.

--- test/integration/uncorkTests.js
+++ test/integration/uncorkTests.js
@@ -3,14 +3,20 @@
 const path = require('path');
 
 const assert = require('assertthat'),
+      defaults = require('lodash/defaults'),
       shell = require('shelljs');
 
 const humanReadableMessage = /^.+ \(.+\)\n.+::.+@\d+\.\d+\.\d+.* \(.+\)\n\d{2}:\d{2}:\d{2}\.\d{3}@\d{4}-\d{2}-\d{2} \d+#\d+\n([^─]*)?─+$/gm;
 
 suite('uncork', () => {
   test('humanizes messages.', done => {
-    shell.exec('node writeMessages.js | node ../../bin/flaschenpost-uncork.js', {
-      cwd: path.join(__dirname, '..', 'helpers')
+    shell.exec('FLASCHENPOST_FORMATTER=json node writeMessages.js | node ../../bin/flaschenpost-uncork.js', {
+      cwd: path.join(__dirname, '..', 'helpers'),
+      /* eslint-disable no-process-env */
+      env: defaults({
+        FLASCHENPOST_FORMATTER: 'human'
+      }, process.env)
+      /* eslint-enable no-process-env */
     }, (code, stdout, stderr) => {
       assert.that(code).is.equalTo(0);
 
