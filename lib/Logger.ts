4d2eb5121771db61cdaa1759795a86088608a7da
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2020-09-02 20:25:47 +0200

    feat: Replace disabled log functions with noops. (#375)
    * feat: Replace disabled log functions with noops.
    
    * feat: Move isDebugFilterEnabled into constructor.
    
    * Fix ESLint errors.
    
    Co-authored-by: Golo Roden <golo.roden@thenativeweb.io>

--- lib/Logger.ts
+++ lib/Logger.ts
@@ -3,6 +3,18 @@ import { LogEntry } from './LogEntry';
 import { LogLevel } from './LogLevel';
 import { PackageJson } from './PackageJson';
 
+const noop = function (): void {
+  // Intentionally left blank.
+};
+
+const logLevelMap = {
+  debug: 5,
+  info: 4,
+  warn: 3,
+  error: 2,
+  fatal: 1
+};
+
 class Logger {
   protected configuration: Configuration;
 
@@ -10,10 +22,39 @@ class Logger {
 
   protected sourcePath: string;
 
+  protected numericLogLevel: number;
+
+  protected isDebugFilterEnabled: boolean;
+
   public constructor (configuration: Configuration, sourcePath: string, packageJson: PackageJson) {
     this.configuration = configuration;
     this.sourcePath = sourcePath;
     this.module = packageJson;
+
+    this.numericLogLevel = logLevelMap[configuration.highestEnabledLogLevel];
+
+    if (this.numericLogLevel < 5) {
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      this.debug = noop;
+    }
+    if (this.numericLogLevel < 4) {
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      this.info = noop;
+    }
+    if (this.numericLogLevel < 3) {
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      this.warn = noop;
+    }
+    if (this.numericLogLevel < 2) {
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      this.error = noop;
+    }
+
+    if (this.configuration.debugModuleFilter.length === 0) {
+      this.isDebugFilterEnabled = true;
+    } else {
+      this.isDebugFilterEnabled = this.configuration.debugModuleFilter.includes(this.module.name);
+    }
   }
 
   public fatal (message: string, metadata?: object): void {
@@ -33,43 +74,14 @@ class Logger {
   }
 
   public debug (message: string, metadata?: object): void {
-    if (!this.isDebugLoggingEnabled()) {
+    if (!this.isDebugFilterEnabled) {
       return;
     }
 
     this.log('debug', message, metadata);
   }
 
-  protected isLogLevelEnabled (logLevel: LogLevel): boolean {
-    if (this.configuration.highestEnabledLogLevel === 'debug') {
-      return [ 'fatal', 'error', 'warn', 'info', 'debug' ].includes(logLevel);
-    }
-    if (this.configuration.highestEnabledLogLevel === 'info') {
-      return [ 'fatal', 'error', 'warn', 'info' ].includes(logLevel);
-    }
-    if (this.configuration.highestEnabledLogLevel === 'warn') {
-      return [ 'fatal', 'error', 'warn' ].includes(logLevel);
-    }
-    if (this.configuration.highestEnabledLogLevel === 'error') {
-      return [ 'fatal', 'error' ].includes(logLevel);
-    }
-
-    return [ 'fatal' ].includes(logLevel);
-  }
-
-  protected isDebugLoggingEnabled (): boolean {
-    if (this.configuration.debugModuleFilter.length === 0) {
-      return true;
-    }
-
-    return this.configuration.debugModuleFilter.includes(this.module.name);
-  }
-
   protected log (logLevel: LogLevel, message: string, metadata?: object): void {
-    if (!this.isLogLevelEnabled(logLevel)) {
-      return;
-    }
-
     const logEntry = new LogEntry(
       this.configuration.logEntryIdGenerator.next().value,
       this.configuration.hostname,
